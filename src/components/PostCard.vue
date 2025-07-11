c24f0bb6a813c331af5cfdbd20e24f30e8dff182
Anas Abdur <anas.rohim.aux@flip.id>
2025-07-11 14:53:58 +0700

    like post function

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,6 +1,10 @@
 <script setup>
 import { ref, computed } from "vue";
+import { useApi } from "@/composables/useApi";
 import { useAsset } from "@/composables/useAsset";
+import { useAuthStore } from "@/stores/auth";
+
+const authStore = useAuthStore();
 
 const { getStorageUrl } = useAsset()
 
@@ -10,8 +14,29 @@ const props = defineProps({
   post: Object,
 });
 
+const isLiked = ref(
+  props.post.likes?.some((like) => like.user_id === authStore.user?.id)
+);
+
 const likeCount = ref(props.post.likes?.length || 0);
 const comments = ref(props.post.comments || []);
+
+const toggleLike = async () => {
+  try {
+    const api = useApi();
+    if (isLiked.value) {
+      await api.delete(`/posts/${props.post.id}/likes`);
+      likeCount.value--;
+    } else {
+      await api.post(`/posts/${props.post.id}/likes`);
+      likeCount.value++;
+    }
+    isLiked.value = !isLiked.value;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 </script>
 
 <template>
@@ -19,8 +44,10 @@ const comments = ref(props.post.comments || []);
     <img :src="postImage" class="post-image" />
 
     <div class="post-actions">
-      <button class="button">
-        ❤️ {{ likeCount }}
+      <button @click="toggleLike" class="button">
+        <span v-if="isLiked">❤️</span>
+        <span v-else>🤍</span>
+        {{ likeCount }}
       </button>
       <button class="button">
         💬 {{ comments.length }}
